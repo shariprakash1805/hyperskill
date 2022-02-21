244f9fb93473b85de57f422a16f7cc570af86587
shariprakash1805 <100148257+shariprakash1805@users.noreply.github.com>
2022-02-21 21:11:38 +0530

    simple bot

---
+++ Java for Beginners/Simple Chatty Bot/Simple Chatty Bot/SimpleBots1.java
@@ -0,0 +1,6 @@
+
+public class SimpleBots1 {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Aid.\nI was created in 2020.");
+    }
+}
---
+++ Java for Beginners/Simple Chatty Bot/Simple Chatty Bot/SimpleBots2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SimpleBots2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.next();
+
+        System.out.println("What a great name you have, "+name+"!");
+    }
+}
